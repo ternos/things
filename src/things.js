0466a2e5ad83879c48ed5181b07364df6c96fde6
Sergei Ternovykh <ternos@yandex-team.ru>
2019-04-06 18:58:13 +0300

    lalal

--- src/things.js
+++ src/things.js
@@ -1,4 +1,21 @@
 window.onload = () => {
 const things = document.querySelectorAll('.Thing')
 console.log(things.length)
+
+for(var i = 0; i < things.length; i++) {
+  things[i].addEventListener('click', function(el) {
+    let tick = things[el].querySelector('.Thing__tick')
+    tick.classList.toggle('Thing__tick_false')
+    tick.classList.toggle('Thing__tick_true')
+    console.dir(things[el].querySelector('.Thing__name').innerHTML)
+  }.bind(this, i));
+}
+
+// keydown keyCode=78 which=78 charCode=0 char=N +ctrl
+window.addEventListener('keydown',(el) => {
+  if(el.ctrlKey === true && el.key === 'n') {
+    console.log("%cNew task!", "color: green; font-style: italic; font-size: 40px");
+  }
+})
+
 }
